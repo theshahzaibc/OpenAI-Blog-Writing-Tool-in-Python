b8cb9cbefbadd961fbbc71b9db58f7adecff7bc2
theshahzaibc <shahzaibsaifullah@gmail.com>
2022-04-08 20:46:41 +0500

    Python version updated

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 OPENAI_API_KEY = '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'
 
 
-
 ## FLASK STUFF
 class Config(object):
     DEBUG = True
     TESTING = False
+
 
 class DevelopmentConfig(Config):
     SECRET_KEY = "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"
